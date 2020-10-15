5f23ded9538956ab5e0693d962f2d983f30a86e5
leeping-ng <leeping_ng@hotmail.com>
2020-10-15 20:29:41 +0800

    completed 1.5

---
+++ 1-5_one_away.py
@@ -0,0 +1,45 @@
+"""
+1.5 One Away
+There are three types of edits that can be performed on strings:
+insert a character, remove a character, or replace a character.
+Given 2 strings, write a function to check if they are one edit (or zero edits) away.
+
+EXAMPLE:
+pale,   ple     -> true
+pales,  pale    -> true
+pale,   bale    -> true
+pale,   bake    -> false
+"""
+
+def optimized(str_a, str_b):
+    if abs(len(str_a) - len(str_b))>1:
+        return False
+
+    if len(str_a) > len(str_b):
+        str_long = str_a
+        str_short = str_b
+    else:
+        str_long = str_b
+        str_short = str_a
+
+    idx_short = 0
+    idx_long = 0
+    diff = 0
+    for i in range(len(str_long)):
+        # if indexes don't tally, skip one and try again next loop, and record 1 diff
+        if str_long[idx_long] != str_short[idx_short]:
+            idx_short -= 1
+            diff += 1
+
+        if diff > 1:
+            return False
+        idx_short += 1
+        idx_long += 1
+
+    return True
+
+if __name__=="__main__":
+    print(optimized('palace', 'place'))
+    print(optimized('pale', 'bake'))
+    print(optimized('rail', 'liar')) # I forgot this scenario initially
+
