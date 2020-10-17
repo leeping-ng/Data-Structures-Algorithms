ec2f1c89e7d9d2cee5b08287323cdd59870d9441
leeping-ng <leeping_ng@hotmail.com>
2020-10-17 09:50:39 +0800

    completed 1.6 optimized stringbuilder

--- 1-6_string_compression.py
+++ 1-6_string_compression.py
@@ -7,6 +7,11 @@ return the original string.
 You can assume the string only has uppercase and lowercase letters (a-z).
 """
 
+"""
+Technique used: String builder - append to a list and use ''.join(str), instead of str += char as it 
+makes copies and takes O(n^2)
+"""
+
 def brute_force(str_input):
     """
     This is very slow because in Python, strings are immutable.
@@ -17,13 +22,11 @@ def brute_force(str_input):
     prior = ''
     count = 1
     for i, char in enumerate(str_input):
-        print(i)
         if char != prior:
             # when alphabet changes, except first case
             if prior != '':
                 str_output += str(count)
             str_output += char
-            print(str_output)
             prior = char
             count = 1
             
@@ -33,10 +36,44 @@ def brute_force(str_input):
             if i == len(str_input)-1:
                 str_output += str(count)
 
-    return str_output
+    if len(str_output) >= len(str_input):
+        return str_input
+    else:
+        return str_output
+
+
+def optimized(str_input):
+    """
+    This is the faster approach as it uses a string builder.
+    """
+    str_output = []
+    prior = ''
+    count = 1
+    for i, char in enumerate(str_input):
+        if char != prior:
+            # when alphabet changes, except first case
+            if prior != '':
+                str_output.append(str(count))
+            str_output.append(char)
+            prior = char
+            count = 1
+            
+        else:
+            count += 1
+            # when reach the end of string, need to append the count too
+            if i == len(str_input)-1:
+                str_output.append(str(count))
+
+    str_output = ''.join(str_output)
+
+    if len(str_output) >= len(str_input):
+        return str_input
+    else:
+        return str_output
 
 if __name__=="__main__":
     print(optimized('aabcccccaaa'))
+    print(optimized('abcdefg'))
 
 
 
